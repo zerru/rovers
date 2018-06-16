920f04f6ce6062a434cc9ae7cd00831557a2f97a
AsierLT <asier.lejarza@gmail.com>
2018-06-16 17:17:15 +0200

    First commit of the project

---
+++ Idiada_Rovers/src/rovers/Input.java
@@ -0,0 +1,23 @@
+package rovers;
+
+import java.util.Scanner;
+
+public class Input {
+	
+	//public void Write() {
+	public Input() {
+		Scanner input = null;
+		try {
+		    input = new Scanner(System.in);
+		    String plateau; 
+			System.out.println("Please, enter the plateau coordinates:");
+			plateau = input.next(); 
+			System.out.println("Plateau : " + plateau);
+		}
+		finally {
+		    if(input!=null)
+		    	input.close();
+		}
+	}
+	
+}
---
+++ Idiada_Rovers/src/rovers/Main.java
@@ -0,0 +1,21 @@
+package rovers;
+
+/**
+* Main
+*/
+public class Main {
+	/**
+	* main
+	*/
+	public static void main(String[] args) {
+		
+		// Create an input object
+        new Input();
+        
+		
+		
+		
+		
+    }
+	
+}
